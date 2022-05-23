f7e861d8ae45b3fbada5497f1df960e1eda15d00
hutnikus <hutnand@gmail.com>
2022-05-23 12:47:57 +0200

    changed file structure

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const express = require("express")
 
 const app = express()
 
+const modulesPath = "./node_modules/easy-educational-games/modules"
+
 //set module folder (client side)
-app.use("/modules",express.static("./node_modules/easy-educational-games/public/modules"))
+app.use("/modules",express.static(this.modulesPath))
 
 const PORT = process.env.PORT || 3000
 
 module.exports = {
-    app,express,PORT,
-    modulesPath: "./node_modules/easy-educational-games/public/modules"
+    app,express,PORT,modulesPath
 }
